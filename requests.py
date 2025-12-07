c0a1374d07c9dc9c2c467640f0d65da7086aa6b6
artcry01 <flagikkanal@gmail.com>
2025-12-08 01:59:51 +0300

    last upd

--- requests.py
+++ requests.py
@@ -19,20 +19,42 @@ async def add_user(tg_id):
         if user:
             return user
         
-        new_user = User(tg_id = tg_id)
+        new_user = User(tg_id=tg_id)
         session.add(new_user)
         await session.commit()
         await session.refresh(new_user)
         return new_user
-    
+
 
 async def get_tasks(user_id):
     async with async_session() as session:
-        tasks = await session.scalars(select(Task).where(Task.user == user_id, Task.completed == False))
-        serialized_tasks = [TaskSchema.model_validate(t).model_dump() for t in tasks]
+        tasks = await session.scalars(
+            select(Task).where(Task.user == user_id, Task.completed == False)
+        )
+        
+        serialized_tasks = [
+            TaskSchema.model_validate(t).model_dump() for t in tasks
+        ]
         
         return serialized_tasks
-    
+
+
 async def get_completed_tasks_count(user_id):
     async with async_session() as session:
-        return await session.scalar(select(func.count(Task.id)).where(Task.completed == True))
+        return await session.scalar(select(func.count(Task.id)).where(Task.completed == True, Task.user == user_id))
+
+
+async def add_task(user_id, title):
+    async with async_session() as session:
+        new_task = Task(
+            title=title,
+            user=user_id
+        )
+        session.add(new_task)
+        await session.commit()
+
+
+async def update_task(task_id):
+    async with async_session() as session:
+        await session.execute(update(Task).where(Task.id == task_id).values(completed=True))
+        await session.commit()
